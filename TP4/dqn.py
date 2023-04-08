be0f18683a7974b0de5544aa6e31baab0b0d23bb
Ilyes Hamdi <ilyes.hamdi99@gmail.com>
2023-04-08 08:21:07 +0200

    new files

---
+++ TP4/dqn.py
@@ -0,0 +1,178 @@
+# Imports
+import sys
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import numpy as np
+import random
+from copy import deepcopy
+import gymnasium as gym
+
+# import os
+# os.environ["SDL_VIDEODRIVER"] = "dummy"
+# from IPython.display import clear_output
+
+import matplotlib.pyplot as plt
+
+
+class ReplayBuffer:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = []
+        self.position = 0
+
+    def push(self, state, action, reward, terminated, next_state):
+        """Saves a transition."""
+        if len(self.memory) < self.capacity:
+            self.memory.append(None)
+        self.memory[self.position] = (state, action, reward, terminated, next_state)
+        self.position = (self.position + 1) % self.capacity
+
+    def sample(self, batch_size):
+        return random.choices(self.memory, k=batch_size)
+
+    def __len__(self):
+        return len(self.memory)
+
+
+class Net(nn.Module):
+    """
+    Basic neural net.
+    """
+
+    def __init__(self, obs_size, hidden_size, n_actions):
+        super(Net, self).__init__()
+        self.net = nn.Sequential(
+            nn.Linear(obs_size, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, n_actions),
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+
+class DQN:
+    def __init__(
+        self,
+        action_space,
+        observation_space,
+        gamma,
+        batch_size,
+        buffer_capacity,
+        update_target_every,
+        epsilon_start,
+        decrease_epsilon_factor,
+        epsilon_min,
+        learning_rate,
+    ):
+        self.action_space = action_space
+        self.observation_space = observation_space
+        self.gamma = gamma
+
+        self.batch_size = batch_size
+        self.buffer_capacity = buffer_capacity
+        self.update_target_every = update_target_every
+
+        self.epsilon_start = epsilon_start
+        self.decrease_epsilon_factor = (
+            decrease_epsilon_factor  # larger -> more exploration
+        )
+        self.epsilon_min = epsilon_min
+
+        self.learning_rate = learning_rate
+
+        self.reset()
+
+    def update(self, state, action, reward, terminated, next_state):
+        """
+        ** SOLUTION **
+        """
+
+        # add data to replay buffer
+        self.buffer.push(
+            torch.tensor(state).unsqueeze(0),
+            torch.tensor([[action]], dtype=torch.int64),
+            torch.tensor([reward]),
+            torch.tensor([terminated], dtype=torch.int64),
+            torch.tensor(next_state).unsqueeze(0),
+        )
+
+        if len(self.buffer) < self.batch_size:
+            return np.inf
+
+        # get batch
+        transitions = self.buffer.sample(self.batch_size)
+
+        (
+            state_batch,
+            action_batch,
+            reward_batch,
+            terminated_batch,
+            next_state_batch,
+        ) = tuple([torch.cat(data) for data in zip(*transitions)])
+
+        values = self.q_net.forward(state_batch).gather(1, action_batch)
+
+        # Compute the ideal Q values
+        with torch.no_grad():
+            next_state_values = (1 - terminated_batch) * self.target_net(
+                next_state_batch
+            ).max(1)[0]
+            targets = next_state_values * self.gamma + reward_batch
+
+        loss = self.loss_function(values, targets.unsqueeze(1))
+
+        # Optimize the model
+        self.optimizer.zero_grad()
+        loss.backward()
+        # torch.nn.utils.clip_grad_value_(self.q_net.parameters(), 100)
+        self.optimizer.step()
+
+        if not ((self.n_steps + 1) % self.update_target_every):
+            self.target_net.load_state_dict(self.q_net.state_dict())
+
+        self.decrease_epsilon()
+
+        self.n_steps += 1
+        if terminated:
+            self.n_eps += 1
+
+        return loss.detach().numpy()
+
+    def get_action(self, state, epsilon=None):
+        """
+        Return action according to an epsilon-greedy exploration policy
+        """
+        if epsilon is None:
+            epsilon = self.epsilon
+
+        if np.random.rand() < epsilon:
+            return env.action_space.sample()
+        else:
+            return np.argmax(self.get_q(state))
+
+    def decrease_epsilon(self):
+        self.epsilon = self.epsilon_min + (self.epsilon_start - self.epsilon_min) * (
+            np.exp(-1.0 * self.n_eps / self.decrease_epsilon_factor)
+        )
+
+    def reset(self):
+        hidden_size = 128
+
+        obs_size = self.observation_space.shape[0]
+        n_actions = self.action_space.n
+
+        self.buffer = ReplayBuffer(self.buffer_capacity)
+        self.q_net = Net(obs_size, hidden_size, n_actions)
+        self.target_net = Net(obs_size, hidden_size, n_actions)
+
+        self.loss_function = nn.MSELoss()
+        self.optimizer = optim.Adam(
+            params=self.q_net.parameters(), lr=self.learning_rate
+        )
+
+        self.epsilon = self.epsilon_start
+        self.n_steps = 0
+        self.n_eps = 0
